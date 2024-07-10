b7c26a6d7b878416c5fa8e16183e77152d904e20
Edilson <edilson@yahoo.com.br>
2024-07-10 11:39:22 -0300

    Adição dos tipos

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
 
-let listaDePets = [];
+import type TipoPet from "../types/TipoPet";
+
+let listaDePets:Array<TipoPet> = [];
 
 export default class PetController {
   criaPet(req: Request, res: Response) {
-    const novoPet = req.body;
+
+    const {id,adotado,especie,idade,nome} = <TipoPet>req.body;
+
+    const novoPet:TipoPet = {id,adotado,especie,idade,nome};
     listaDePets.push(novoPet);
     return res.status(201).json(novoPet);
   }
---
+++ src/types/TipoPet.ts
@@ -0,0 +1,8 @@
+type TipoRet={
+    id:Number,
+    nome:String,
+    especie:String,
+    adotado:Boolean,
+    idade:Number
+}
+export default TipoRet;
